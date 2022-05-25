03361b1a21870ffcc6a001a3254e10fc06c842e7
Niel Swart <niel@nielswrat.com>
2022-05-25 12:26:33 +0200

    fix: Fix typo

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.write("Welcom to the crop nutrient recommendation tool!")
+st.write("Welcome to the crop nutrient recommendation tool!")
